7560984090b68e56260dd87bc97ebe66294ee205
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-03-08 21:02:59 -0800

    updated docs

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Address represents a mailing address.
+Pacakge staddr provides tools for dealing with mailing addresses, such splitting up a street address to building number, street name, and street type.
 
 Likely you would load the fields of this, and then call
 the normalize method, perhaps to split up a street address
